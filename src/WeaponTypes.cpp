ca4960a4c7989da636d24b295ee573897ae7949f
guitar-mk <markus-kriener@web.de>
2026-02-26 21:03:12 +0100

    clean

--- src/WeaponTypes.cpp
+++
@@ -1,9 +0,0 @@
-#include "WeaponTypes.h"
-
-Ammo::Ammo(std::string n, int w, int h, std::string type, int amt)
-    : Item(n, w, h), ammoType(type), amount(amt) {}
-
-void Ammo::inspect() const {
-    std::cout << "- [MUNITION] " << name << " (" << amount << " Schuss " 
-              << ammoType << ") (" << gridWidth << "x" << gridHeight << " Slots)\n";
-}
